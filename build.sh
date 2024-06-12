3ac9172e44cac43ce8a9bbe0bfbe8ba197ff2b3e
moss <geekzsp@gmail.com>
2024-06-12 17:55:28 +0800

    fix:build script

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 
+echo $SCRIPT_DIR
 "./gradlew" assembleRelease --no-daemon
 
 "$SCRIPT_DIR/jar/genJar.sh" "$1"
--- jar/genJar.sh
+++ jar/genJar.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-SCRIPT_DIR="/Users/moss/workspace/private/CatVodSpider/jar"
-
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+cd ..
+ROOT_DIR="$(pwd)"
+echo  $SCRIPT_DIR
+echo  $ROOT_DIR
 # 删除文件和目录
 rm -f "$SCRIPT_DIR/custom_spider.jar"
 rm -rf "$SCRIPT_DIR/Smali_classes"
 
 # 反编译 dex 文件到 smali
-java -jar "$SCRIPT_DIR/3rd/baksmali-2.5.2.jar" d "/Users/moss/workspace/private/CatVodSpider/app/build/intermediates/dex/release/minifyReleaseWithR8/classes.dex" -o "$SCRIPT_DIR/Smali_classes"
+java -jar "$SCRIPT_DIR/3rd/baksmali-2.5.2.jar" d "$ROOT_DIR/app/build/intermediates/dex/release/minifyReleaseWithR8/classes.dex" -o "$SCRIPT_DIR/Smali_classes"
 
 # 删除旧的 smali 目录
 rm -rf "$SCRIPT_DIR/spider.jar/smali/com/github/catvod/spider"
