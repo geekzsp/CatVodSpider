b61ef3eec9e351fefed5d4ba99b565f8d9fb007e
Moss <geekzsp@gmail.com>
2024-06-12 17:39:36 +0800

    Add files via upload

---
+++ jar/genJar.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+SCRIPT_DIR="/Users/moss/workspace/private/CatVodSpider/jar"
+
+# 删除文件和目录
+rm -f "$SCRIPT_DIR/custom_spider.jar"
+rm -rf "$SCRIPT_DIR/Smali_classes"
+
+# 反编译 dex 文件到 smali
+java -jar "$SCRIPT_DIR/3rd/baksmali-2.5.2.jar" d "/Users/moss/workspace/private/CatVodSpider/app/build/intermediates/dex/release/minifyReleaseWithR8/classes.dex" -o "$SCRIPT_DIR/Smali_classes"
+
+# 删除旧的 smali 目录
+rm -rf "$SCRIPT_DIR/spider.jar/smali/com/github/catvod/spider"
+rm -rf "$SCRIPT_DIR/spider.jar/smali/com/github/catvod/parser"
+rm -rf "$SCRIPT_DIR/spider.jar/smali/com/github/catvod/js"
+
+# 创建目录
+mkdir -p "$SCRIPT_DIR/spider.jar/smali/com/github/catvod/"
+
+# 移动 smali 文件
+mv "$SCRIPT_DIR/Smali_classes/com/github/catvod/spider" "$SCRIPT_DIR/spider.jar/smali/com/github/catvod/"
+mv "$SCRIPT_DIR/Smali_classes/com/github/catvod/parser" "$SCRIPT_DIR/spider.jar/smali/com/github/catvod/"
+mv "$SCRIPT_DIR/Smali_classes/com/github/catvod/js" "$SCRIPT_DIR/spider.jar/smali/com/github/catvod/"
+
+# 重新编译
+java -jar "$SCRIPT_DIR/3rd/apktool_2.4.1.jar" b "$SCRIPT_DIR/spider.jar" -c
+
+# 移动输出文件并生成 MD5
+mv "$SCRIPT_DIR/spider.jar/dist/dex.jar" "$SCRIPT_DIR/custom_spider.jar"
+
+md5 -q "$SCRIPT_DIR/custom_spider.jar" > "$SCRIPT_DIR/custom_spider.jar.md5"
+
+# 删除临时目录
+rm -rf "$SCRIPT_DIR/spider.jar/build"
+rm -rf "$SCRIPT_DIR/spider.jar/smali"
+rm -rf "$SCRIPT_DIR/spider.jar/dist"
+rm -rf "$SCRIPT_DIR/Smali_classes"
